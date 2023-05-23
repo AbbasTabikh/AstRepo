15fbdf1c2d7d72e0cee1d3bbaa7191988b599fc8
Abbass Tabikh <abbasstabikh87774@gmail.com>
2023-05-23 17:44:29 +0300

    Municipality Search

---
+++ Demo.Api/Extension Methods/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Demo.Api.Extension_Methods
+{
+    public static class StringExtension
+    {
+
+        public static bool IsEnglishLetterOrWhiteSpace(this char value)
+        {
+            return char.IsUpper(value) || char.IsLower(value) || char.IsWhiteSpace(value);
+        }
+
+
+        public static bool IsEnglish(this string value)
+        {
+           return value.All(c => c.IsEnglishLetterOrWhiteSpace());
+        }
+
+
+    }
+}
--- Demo.Api/Services/MunicipalityService.cs
+++ Demo.Api/Services/MunicipalityService.cs
@@ -1,6 +1,8 @@
-﻿using Demo.Data.Data;
+﻿using Demo.Api.Extension_Methods;
+using Demo.Data.Data;
 using Demo.Data.Models;
 using FuzzySharp;
+using FuzzySharp.SimilarityRatio.Scorer;
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Api.Services
@@ -17,7 +19,49 @@ namespace Demo.Api.Services
         public async Task<IEnumerable<Municipality>> SearchByNameAsync(string name , CancellationToken cancellationToken)
         {
 
-            
+            if(string.IsNullOrEmpty(name))
+            {
+                return Enumerable.Empty<Municipality>();
+            }
+
+
+            IEnumerable<string> choices = null;
+
+            bool isInEnglsih = false;
+
+            if (name.IsEnglish())
+            {
+                choices = await _dataContext.Municipalities.Select(x => x.EnglishName).ToArrayAsync(cancellationToken);
+                isInEnglsih = true;
+            }
+
+            //Arabic
+            else
+            {
+                choices = await _dataContext.Municipalities.Select(x => x.ArabicName).ToArrayAsync(cancellationToken);
+            }
+
+            var result = Process.ExtractTop(name, choices, null, null, 15, 50);
+            var matchedOptions = result.Select(r => r.Value).ToArray();
+            var resultQuery = Array.Empty<Municipality>();
+
+            if(isInEnglsih)
+            {
+               resultQuery = _dataContext.Municipalities
+                                         .Where(e => matchedOptions.Contains(e.EnglishName))
+                                         .ToArray();
+            }
+
+            else
+            {
+              resultQuery = _dataContext.Municipalities
+                                        .Where(e => matchedOptions.Contains(e.ArabicName))
+                                        .ToArray();
+            }
+
+
+            return resultQuery;
+
         }
 
 
